44e231ac4fbb395ece8bd13c2a6084f771f8055e
chunchi851214 <77277705+chunchi851214@users.noreply.github.com>
2022-11-20 15:30:52 +0800

    Update to remove files with keywords.

--- autoDeleteFile.py
+++ autoDeleteFile.py
@@ -5,13 +5,13 @@ def delete_files(part_file_name, rootdir = './'):
     count = 0
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
-            if file[-len(part_file_name):] == part_file_name:
+            if part_file_name in file:
                 count += 1
                 os.remove(os.path.join(subdir, file))
                 print("%s is removed" % file)
     return count
 #%%
-part_file_name = input("輸入想刪除檔案的關鍵字 (Please enter the keywords in the file you wanna remove): ")
+part_file_name = input("Enter the keywords of files that would be removed: ")
 count = delete_files(part_file_name)
 print("Totally remove {} files".format(count))
 os.system('pause')
